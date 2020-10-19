07b7d53cd10587145947155d0ebecf17d6572363
SNEHA MALLIK <snehamallik665@gmail.com>
2020-10-19 12:40:35 +0530

    sum of two integers added

---
+++ Cpp_codes/addTwoIntegers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,c;
+	cout<<"Enter first number: \n";
+	cin>>a;
+	cout<<"Enter second number: \n";
+	cin>>b;
+	c=a+b;
+	cout<<"Sum of integers: "<<c;
+	return 0;
+}
+
