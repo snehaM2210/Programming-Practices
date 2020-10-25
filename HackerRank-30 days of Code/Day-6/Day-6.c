1e649e1bc15448c0ff863d93d842f9bd7306318e
SNEHA MALLIK <snehamallik665@gmail.com>
2020-10-25 13:46:48 +0530

    Day-6 code in C added

---
+++ HackerRank-30 days of Code/Day-6/Day-6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int n,i; char s[10000];
+    scanf("%d",&n);  
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",s);
+        myFunction(s);
+    }
+}
+void myFunction(char s[],int i)
+{            
+        for(i=0;i<strlen(s);i++){
+            if(i%2==0){
+                printf("%c",s[i]);
+            }
+        }
+        printf(" ");
+        for(i=0;i<strlen(s);i++){
+            if(i%2!=0){
+                printf("%c",s[i]);
+            }
+        }
+        printf("\n");
+    return 0;
+}
+
