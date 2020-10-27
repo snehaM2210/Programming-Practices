ce97f3b6cf4e277aac36cddd180c46eaf1eb55b2
SNEHA MALLIK <snehamallik665@gmail.com>
2020-10-27 18:05:58 +0530

    Day-9 code in cpp added

---
+++ HackerRank-30 days of Code/Day-9/Day-9.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the factorial function below.
+int factorial(int n) {
+    if(n==1)
+    return 1;
+    else
+    return n*factorial(n-1);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout<<factorial(n)<<endl;
+
+    return 0;
+}
+
