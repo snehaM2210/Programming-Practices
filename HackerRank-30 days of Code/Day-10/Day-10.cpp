6f282aa8fa06dd00b3ce6f2cf9f1c2795948181e
SNEHA MALLIK <snehaM2210@github.com>
2020-10-28 22:41:57 +0530

    Day-10 code in cpp added

---
+++ HackerRank-30 days of Code/Day-10/Day-10.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int sum=0,max=0;
+    while(n>0)
+    {
+        if(n%2==1)
+        {
+            sum++;
+            if(sum>max)
+            max=sum;
+        }
+        else
+        {
+            sum=0;
+            n=n/2;
+        }
+        return 0;
+    }
+}
