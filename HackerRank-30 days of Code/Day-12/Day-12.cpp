5ce18d8f1e75286132f0652560e66cb4bfdb72b4
Sneha Mallik <snehaM2210@github.com>
2020-10-30 21:13:12 +0530

    Day-12 code in cpp added

---
+++ HackerRank-30 days of Code/Day-12/Day-12.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Person{
+	protected:
+		string firstName;
+		string lastName;
+		int id;
+	public:
+		Person(string firstName, string lastName, int identification){
+			this->firstName = firstName;
+			this->lastName = lastName;
+			this->id = identification;
+		}
+		void printPerson(){
+			cout<< "Name: "<< lastName << ", "<< firstName <<"\nID: "<< id << "\n"; 
+		}
+	
+};
+
+class Student :  public Person{
+	private:
+		vector<int> testScores;  
+	public:
+        Student(string firstName, string lastName, int identification, vector<int> scores) : Person(firstName, lastName, identification), testScores(scores) { }
+        char calculate(){
+            int average=0;
+            for(int i=0;i<testScores.size();i++){
+                average+=testScores[i];
+            }
+            average=average/testScores.size();
+            if(average>=90){
+                return 'O';//O=outstanding
+            } 
+            else if(average>=80){
+                return 'E';//E=exceeds expectations
+            }
+            else if(average>=70){
+                return 'A';//A=Acceptable
+            }
+            else if(average>=55){
+                return 'P';//P=Poor
+            }
+            else if(average>=40){
+                return 'D';//D=Dreadful
+            }
+            else{
+                return 'T';//T=troll
+            }
+        }
+};
+
+int main() {
+	string firstName;
+  	string lastName;
+	int id;
+  	int numScores;
+	cin >> firstName >> lastName >> id >> numScores;
+  	vector<int> scores;
+  	for(int i = 0; i < numScores; i++){
+	  	int tmpScore;
+	  	cin >> tmpScore;
+		scores.push_back(tmpScore);
+	}
+	Student* s = new Student(firstName, lastName, id, scores);
+	s->printPerson();
+	cout << "Grade: " << s->calculate() << "\n";
+	return 0;
+}
