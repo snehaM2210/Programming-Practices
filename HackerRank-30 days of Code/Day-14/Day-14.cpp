523c7fe164f76c66a82f921e0b843fbfc121d1dd
Sneha Mallik <snehamallik665@gmail.com>
2020-11-01 16:24:19 +0530

    Day-14 code in cpp added

---
+++ HackerRank-30 days of Code/Day-14/Day-14.cpp
@@ -0,0 +1,45 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+
+	// Adding my code here
+    Difference(vector<int> ele) : elements(ele) {}
+    void computeDifference(){
+        sort(elements.begin(), elements.end(), greater<int>());
+        maximumDifference = elements.at(0) - elements.at(elements.size() - 1);
+    }
+
+}; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
