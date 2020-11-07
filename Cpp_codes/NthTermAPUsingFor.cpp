8e18857d76d8715e22fbaca068d8db0544cfd5c1
Sneha Mallik <snehamallik665@gmail.com>
2020-11-07 13:44:28 +0530

    Using For loop,sum of n terms of AP added

---
+++ Cpp_codes/NthTermAPUsingFor.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,a,d,an=0,sn=0;
+	cout<<"No. of terms: ";
+	cin>>n;
+	cout<<"\nEnter the first term: ";
+	cin>>a;
+	cout<<"\nEnter the difference: ";
+	cin>>d;
+	for(int i=1;i<=n;i++){
+		an=a+((i-1)*d);
+		cout<<"Term"<<i<<" = "<<an<<endl;
+		sn=sn+an;
+	}
+	cout<<"\nSum of Nth terms = "<<sn<<endl;
+	return 0;
+}
