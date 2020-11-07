256fd528298442055f931b7f14e390994a58a779
Sneha Mallik <snehamallik665@gmail.com>
2020-11-07 20:34:12 +0530

    Pattern 4 in cpp added

---
+++ Cpp_codes/Pattern4.cpp
@@ -0,0 +1,30 @@
+//To print a pattern of asterick as below
+/*
+For n=5;
+
+1
+12
+123
+1234
+12345
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,i,j;
+	cout<<"Enter no. of rows: ";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			cout<<j;
+		}
+		j++;
+		cout<<endl;
+	}
+	return 0;
+}
