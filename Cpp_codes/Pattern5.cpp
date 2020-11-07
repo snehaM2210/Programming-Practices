638dd2abe76bee76eef2b02e1df118ab95c5050e
Sneha Mallik <snehamallik665@gmail.com>
2020-11-07 21:01:54 +0530

    Pattern 5 in cpp added

---
+++ Cpp_codes/Pattern5.cpp
@@ -0,0 +1,30 @@
+//To print a pattern of asterick as below
+/*
+For n=5;
+
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,i,j,k=1;
+	cout<<"Enter no. of rows: ";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			cout<<k<<" ";
+			k++;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
