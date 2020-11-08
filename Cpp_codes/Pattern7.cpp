0a7154164eeb9efdb3873808b18b37132e096ab6
Sneha Mallik <snehamallik665@gmail.com>
2020-11-08 14:18:46 +0530

    Pattern 7 in cpp added

---
+++ Cpp_codes/Pattern7.cpp
@@ -0,0 +1,33 @@
+//To print a pattern of asterick as below
+/*
+For n=5;
+	 *
+    ***
+   *****
+  *******
+ *********
+
+*/
+
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+int i,j,n,k=0;    
+cout<<"Enter the no. of Rows: ";    
+cin>>n;    
+for(i=1,k=0;i<=n;i++,k=0)    
+{    
+	for(j=1;j<=(n-i);j++)    
+	{
+		cout<<" ";
+	}
+	while(k!=(2*i)-1)
+	{
+		cout<<"*";
+		k++;
+	}				
+cout<<endl;    
+}    
+return 0;  
+} 
