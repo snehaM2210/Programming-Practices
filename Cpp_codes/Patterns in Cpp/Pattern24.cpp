29b48f29df3d2d46e042b53281af3bab0a327cd6
Sneha Mallik <snehamallik665@gmail.com>
2020-12-09 15:46:14 +0530

    Added pattern 24

--- Cpp_codes/Patterns in Cpp/Pattern24.cpp
+++ Cpp_codes/Patterns in Cpp/Pattern24.cpp
@@ -10,3 +10,24 @@ Taking n=5,you can print the following pattern
             
 */
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter no. of rows: ";
+    cin>>n;
+    for(int i=0;i>=1;i--){
+        for(int k=0;k<=n-1;k++){
+            cout<<"  ";
+        }
+        for(int j=i;j<=2*i-1;j++){
+            cout<<"* ";
+        }
+        for(int j=0;j<i-1;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
