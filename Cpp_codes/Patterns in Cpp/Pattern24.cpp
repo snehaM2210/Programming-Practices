bc6c14f4a1bbb4d07e7204ef8a5e471b1473048b
Sneha Mallik <snehamallik665@gmail.com>
2020-12-09 14:55:08 +0530

    Create Pattern24.cpp

---
+++ Cpp_codes/Patterns in Cpp/Pattern24.cpp
@@ -0,0 +1,12 @@
+//To print a pattern of asterick as below(Pascal's Triangle)
+/*
+Taking n=5,you can print the following pattern
+
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+            
+*/
+
