7b8b93fc93193011ea799023f550dd949cf0170b
snehaM2210 <snehamallik665@gmail.com>
2021-01-23 23:41:15 +0530

    Calculate the average of age of people taken as input

---
+++ Cpp_codes/ageAverage.cpp
@@ -0,0 +1,18 @@
+//sentinel controlled program
+#include <iostream>
+using namespace std;
+
+int main() {
+	int age,ageTotal=0,noOfPeople=0;
+	cout<<"Enter 1st age or -1 to quit"<<endl;
+	cin>>age;
+	while(age!= -1){
+		ageTotal=ageTotal+age;
+		noOfPeople++;
+		cout<<"Enter next person's age or -1 to quit"<<endl;
+		cin>>age;
+	}
+	cout<<"No. of people entered: "<<noOfPeople<<endl;
+	cout<<"Average age: "<<ageTotal/noOfPeople;
+	return 0;
+}
