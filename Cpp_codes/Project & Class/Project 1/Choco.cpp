d803457a835e7cd91f7e32252599c5e622e65de9
snehaM2210 <snehamallik665@gmail.com>
2021-02-03 20:49:32 +0530

    Create Choco.cpp

---
+++ Cpp_codes/Project & Class/Project 1/Choco.cpp
@@ -0,0 +1,10 @@
+#include "Choco.h"
+#include <iostream>
+using namespace std;
+
+Choco::Choco(){  //constructor class
+}
+
+void Choco::printCrap(){  //classname::function name ,means printCrap belongs to Class Choco
+	cout<<"Did someone said biriyani?"<<endl;
+}
