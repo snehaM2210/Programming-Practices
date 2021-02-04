c57f89b6563cfe13b454501d14bb75c0e657be12
snehaM2210 <snehamallik665@gmail.com>
2021-02-04 18:20:18 +0530

    Create NumberMirror.cpp

---
+++ CodeChef/Beginner Practice/NumberMirror.cpp
@@ -0,0 +1,27 @@
+// Problem Statement
+// Write a program that accepts a number, n, and outputs the same.
+
+// Input
+// The only line contains a single integer.
+
+// Output
+// Output the answer in a single line.
+
+// Constraints
+// 0 ≤ n ≤ 105
+// Sample Input
+// 123
+// Sample Output
+// 123
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//Take a no. as input
+	int n;
+	cin>>n;
+	//To give the same no. as output
+	cout<<n;
+	return 0;
+}
