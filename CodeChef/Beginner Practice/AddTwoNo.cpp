77459d4d9c58de65fa8bdbddf3ff88e741626e96
snehaM2210 <snehamallik665@gmail.com>
2021-02-04 17:45:17 +0530

    Update AddTwoNo.cpp

--- CodeChef/Beginner Practice/AddTwoNo.cpp
+++ CodeChef/Beginner Practice/AddTwoNo.cpp
@@ -28,14 +28,16 @@ using namespace std;
 
 int main() {
 	// Read the number of test cases.
-	int T,A,B,i,sum[i];
+	int T;
 	cin>>T;
-	for(i=1;i<=T;i++){
-	    cin>>A>>B;
-	    sum[i]=A+B;
-	}
-	for(int i=1;i<=T;i++){
-	    cout<<sum[i]<<endl;
+	while (T--) {
+		// Read the input a, b
+		int a, b;
+		cin>>a>>b;
+
+		// Compute the ans.
+		int ans = a + b;
+		cout<<ans<<endl;
 	}
 	return 0;
 }
