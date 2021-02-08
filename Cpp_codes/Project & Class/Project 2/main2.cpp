f281adfdc93c2755f0dde5925697ce37f8c9ef0e
snehaM2210 <snehamallik665@gmail.com>
2021-02-08 19:48:46 +0530

    Create main2.cpp

---
+++ Cpp_codes/Project & Class/Project 2/main2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Sally.h"
+using namespace std;
+
+int main() {
+	Sally so;//constructor called //so-objectname
+	cout<<"Omg...Wtf is it on my hand?"<<endl;
+	//after the program finishes deconstructor is called without 
+	//specially calling them
+	return 0;
+}
+
+
