7cc5919a2f5403747269b9e53c3bcb9299645fde
snehaM2210 <snehamallik665@gmail.com>
2021-02-08 19:48:51 +0530

    Create Sally.cpp

---
+++ Cpp_codes/Project & Class/Project 2/Sally.cpp
@@ -0,0 +1,11 @@
+#include "Sally.h"
+#include <iostream>
+using namespace std;
+
+Sally::Sally(){
+	cout<<"I am the constructor."<<endl;
+}
+
+Sally::~Sally(){
+	cout<<"I am the deconstructor!"<<endl;
+}
