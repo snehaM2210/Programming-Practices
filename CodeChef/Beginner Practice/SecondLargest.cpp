043dab632255e0b4de268cc0dda1e0663abf514a
snehaM2210 <snehamallik665@gmail.com>
2021-02-12 22:23:57 +0530

    Create SecondLargest.cpp

---
+++ CodeChef/Beginner Practice/SecondLargest.cpp
@@ -0,0 +1,46 @@
+// Three numbers A, B and C are the inputs. Write a program to find second largest among them.
+
+// Input
+// The first line contains an integer T, the total number of testcases. Then T lines follow, each line contains three integers A, B and C.
+
+// Output
+// For each test case, display the second largest among A, B and C, in a new line.
+
+// Constraints
+// 1 ≤ T ≤ 1000
+// 1 ≤ A,B,C ≤ 1000000
+// Example
+// Input
+// 3 
+// 120 11 400
+// 10213 312 10
+// 10 3 450
+
+// Output
+
+// 120
+// 312
+// 10
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// no. of test cases
+	int T;
+	cin>>T;
+	while(T--){
+	    int A,B,C;
+	    cin>>A>>B>>C;
+	    if(A>B && B>C || C>B && B>A){
+	        cout<<B<<endl;
+	    }
+	    else if(B>C && C>A || A>C && C>B){
+	        cout<<C<<endl;
+	    }
+	    else if(C>A && A>B || B>A && A>C){
+	        cout<<A<<endl;
+	    }
+	}
+	return 0;
+}
