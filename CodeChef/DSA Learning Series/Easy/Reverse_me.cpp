1cea4704fadd525674bc026f3365f6f2de59770b
snehaM2210 <snehamallik665@gmail.com>
2021-04-29 15:33:23 +0530

    Create Reverse_me.cpp

---
+++ CodeChef/DSA Learning Series/Easy/Reverse_me.cpp
@@ -0,0 +1,43 @@
+// You are given a list of N integers and you need to reverse it and print the reversed list in a new line.
+
+// Input:
+// First-line will contain the number N.
+// Second line will contain N space-separated integers.
+// Output:
+// Print the reversed list in a single line.
+
+// Constraints
+// 1≤N,Ai≤105
+// Sample Input 1:
+// 4
+// 1 3 2 4
+// Sample Output 1:
+// 4 2 3 1
+// Sample Input 2:
+// 2
+// 9 8
+// Sample Output 2:
+// 8 9
+// EXPLANATION:
+// In the first example, the reverse of the [1,3,2,4] is [4,2,3,1].
+// In the second example, the reverse of [9,8] is [8,9].
+
+#include <iostream>
+using namespace std;
+
+void reverse_me(int *arr,int n){
+    for(int i=n-1;i>=0;i--){
+        cout<<arr[i]<<" ";
+    }
+}
+int main() {
+	int n;
+	cin>>n;
+	int *arr=new int[n];
+	for(int i=0;i<n;i++){
+	    cin>>arr[i];
+	}
+	reverse_me(arr,n);
+	delete [] arr;
+	return 0;
+}
