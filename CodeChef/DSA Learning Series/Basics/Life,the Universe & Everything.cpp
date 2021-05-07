92d505471bf6420ce905501e9252e73ea37c7bbe
snehaM2210 <snehamallik665@gmail.com>
2021-05-07 17:17:51 +0530

    Create Life,the Universe & Everything.cpp

---
+++ CodeChef/DSA Learning Series/Basics/Life,the Universe & Everything.cpp
@@ -0,0 +1,27 @@
+// Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything. More precisely… rewrite small numbers from input to output. Stop processing input after reading in the number 42. All numbers at input are integers of one or two digits.
+
+// Sample Input:
+// 1
+// 2
+// 88
+// 42
+// 99
+// Sample Output:
+// 1
+// 2
+// 88
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    while(true){
+        cin>>n;
+        if(n==42){
+            break;
+        }
+        cout<<n<<endl;
+    }
+	return 0;
+}
