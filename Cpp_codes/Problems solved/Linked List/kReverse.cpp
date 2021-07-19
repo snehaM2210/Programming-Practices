812174372d5b2027d476c3815adb69c97ac1135f
snehaM2210 <snehamallik665@gmail.com>
2021-07-19 21:21:21 +0530

    Update kReverse.cpp

--- Cpp_codes/Problems solved/Linked List/kReverse.cpp
+++ Cpp_codes/Problems solved/Linked List/kReverse.cpp
@@ -3,10 +3,9 @@
 //  'k' is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of 'k,' then left-out nodes, in the end, should be reversed as well.
 // Example :
 // Given this linked list: 1 -> 2 -> 3 -> 4 -> 5
-
 // For k = 2, you should return: 2 -> 1 -> 4 -> 3 -> 5
-
 // For k = 3, you should return: 3 -> 2 -> 1 -> 5 -> 4
+
 //  Note :
 // No need to print the list, it has already been taken care. Only return the new head to the list.
 //  Input format :
