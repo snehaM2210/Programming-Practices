2eeb795a3a026efc7123ef1311d94185cc040cd1
snehaM2210 <snehamallik665@gmail.com>
2021-07-23 22:32:23 +0530

    Update StackUsingLL.cpp

--- Cpp_codes/Problems solved/Stacks and Queues/StackUsingLL.cpp
+++ Cpp_codes/Problems solved/Stacks and Queues/StackUsingLL.cpp
@@ -108,9 +108,61 @@ public:
     }
 };
 
+/*-----------------------Using template-----------------------------
+
+template<typename T>
+class Stack{
+    Node<T> *head;
+    int size; //no. of elements present in stack
+
+    public:
+
+    Stack(){
+        head=NULL;
+        size=0;
+    }
+
+    int getSize(){
+        return size;
+    }
+
+    bool isEmpty(){
+        return size==0;
+    }
+
+    void push(T element){
+        Node<T> *newNode = new Node<T>(element);
+        newNode->next=head;
+        head=newNode;
+        size++;
+    }
+
+    T pop(){
+        if(isEmpty()){
+            return 0;
+        }
+        T ans=head->data;
+        Node<T> *temp=head;
+        head=head->next;
+        delete temp;
+        size--;
+        return ans;
+    }
+
+    T top(){
+        if(isEmpty()){
+            return 0;
+        }
+        return head->data;
+    }
+}    
+-----------------------------------------------------------*/
+
 int main()
 {
     Stack st;
+    //For using template,we should mention here datatype, For e.g.
+    //Stack<int> st;
 
     int q;
     cin >> q;
