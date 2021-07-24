183f8c13735d446e356870bb695d56c15378b5a1
snehaM2210 <snehamallik665@gmail.com>
2021-07-24 22:34:32 +0530

    Create BalancedParenthesis.cpp

---
+++ Cpp_codes/Problems solved/Stacks and Queues/BalancedParenthesis.cpp
@@ -0,0 +1,87 @@
+// Code : Balanced Parenthesis
+// For a given a string expression containing only round brackets or parentheses, check if they are balanced or not. Brackets are said to be balanced if the bracket which opens last, closes first.
+// Example:
+// Expression: (()())
+// Since all the opening brackets have their corresponding closing brackets, we say it is balanced and hence the output will be, 'true'.
+// You need to return a boolean value indicating whether the expression is balanced or not.
+// Note:
+// The input expression will not contain spaces in between.
+// Input Format:
+// The first and the only line of input contains a string expression without any spaces in between.
+//  Output Format:
+// The only line of output prints 'true' or 'false'.
+// Note:
+// You don't have to print anything explicitly. It has been taken care of. Just implement the function.
+// Constraints:
+// 1 <= N <= 10^7
+//  Where N is the length of the expression.
+
+// Time Limit: 1sec
+// Sample Input 1 :
+// (()()())
+// Sample Output 1 :
+// true
+// Sample Input 2 :
+// ()()(()
+// Sample Output 2 :
+// false
+// Explanation to Sample Input 2:
+// The initial two pairs of brackets are balanced. But when you see, the opening bracket at the fourth index doesn't have its corresponding closing bracket which makes it imbalanced and in turn, making the whole expression imbalanced. Hence the output prints 'false'.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include <stack>
+bool isBalanced(string expression)
+{
+    stack<char> s;
+    char t;
+    for (int i = 0; i < expression.length(); i++)
+    {
+        if (expression[i] == '(' || expression[i] == '[' || expression[i] == '{')
+        {
+            s.push(expression[i]);
+            continue;
+        }
+        if (s.empty())
+        {
+            return false;
+        }
+        switch (expression[i])
+        {
+        case ')':
+            t = s.top();
+            s.pop();
+            if (t == '{' || t == '[')
+            {
+                return false;
+            }
+            break;
+        case '}':
+            t = s.top();
+            s.pop();
+            if (t == '(' || t == '[')
+            {
+                return false;
+            }
+            break;
+        case ']':
+            t = s.top();
+            s.pop();
+            if (t == '(' || t == '{')
+            {
+                return false;
+            }
+            break;
+        }
+    }
+    return (s.empty());
+}
+
+int main()
+{
+    string input;
+    cin >> input;
+    cout << ((isBalanced(input)) ? "true" : "false");
+}
