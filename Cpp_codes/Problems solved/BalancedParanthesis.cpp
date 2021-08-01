fd5c95dcd1c6189373d02188805e86cd50e53353
snehaM2210 <snehamallik665@gmail.com>
2021-08-01 09:47:01 +0530

    Added BalancedParanthesis.cpp

---
+++ Cpp_codes/Problems solved/BalancedParanthesis.cpp
@@ -0,0 +1,42 @@
+//All possible valid parenthesis for given n
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> v;
+
+void generate(string &s, int open, int close)
+{
+    if (open == 0 & close == 0)
+    {
+        v.push_back(s);
+        return;
+    }
+    if (open > 0)
+    {
+        s.push_back('(');
+        generate(s, open - 1, close);
+        s.pop_back();
+    }
+    if (close > 0)
+    {
+        if (open < close)
+        {
+            s.push_back(')');
+            generate(s, open, close - 1);
+            s.pop_back();
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    string s;
+    generate(s, n, n);
+    for (auto ele : v)
+    {
+        cout << ele << endl;
+    }
+}
