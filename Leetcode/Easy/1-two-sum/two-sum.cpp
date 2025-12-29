d3207e9372ee08f3eab21f8ccdb8ac2296c114fb
SNEHA MALLIK <snehamallik665@gmail.com>
2025-12-29 23:59:47 +0530

    Time: 4 ms (53.35%) | Memory: 14.9 MB (37.04%) - LeetSync

--- Leetcode/Easy/1-two-sum/two-sum.cpp
+++ Leetcode/Easy/1-two-sum/two-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        /*
         for(int i=0;i<nums.size(); i++){
             for(int j=i+1;j<nums.size();j++){
                 if(nums[i]+nums[j] == target){
@@ -8,6 +9,39 @@ public:
                 }
             }
         }
+        return {};*/
+
+        /* //Works only on sorted arrays
+        int left=0;
+        int right=nums.size()-1;
+
+        while(left<right){
+            int currentSum = nums[left] + nums[right];
+            if(currentSum == target){
+                return {left,right};
+            }
+            if(currentSum < target){ 
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return {};*/
+        unordered_map <int, int> m;
+        // a+b = target, then a = target-b or b = target-a
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+
+            // If the complement is in the map, we found the pair!
+            if (m.find(complement) != m.end()) {
+                return {m[complement], i};
+            }
+
+            // Otherwise, save this number and its index for later
+            m[nums[i]] = i;
+        }
+
         return {};
     }
 };
